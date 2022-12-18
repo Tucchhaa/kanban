b1123e2c682947852f357b6d0ae4fcd55a0ace9b
Eldar Iusupzhanov <eldar.iusupzhanov@devexpress.com>
2022-12-18 19:06:43 +0800

    Draggable column: separate component and view

--- src/components/column.draggable.component.ts
+++ src/components/column.draggable.component.ts
@@ -3,6 +3,7 @@ import { BaseComponent } from "../base/component";
 import { DragController } from "../drag-drop/drag.controller";
 import { Column } from "../types";
 import { DragState } from "../drag-drop/drag.state";
+import { DraggableColumnView } from "../column/column.drag-wrapper.view";
 
 export class DraggableColumnComponent extends BaseComponent<ColumnOptions, ColumnState, DraggableColumnView> {
     constructor(container: HTMLElement | null, columnOptions: ColumnOptions) {
@@ -12,26 +13,5 @@ export class DraggableColumnComponent extends BaseComponent<ColumnOptions, Colum
         this.registerController(() => new DragController<Column>(this.state.column));
 
         super.render();
-    }
-}
-
-// ===
-
-import { DragView } from "../drag-drop/drag.view";
-import { ColumnComponent } from "./column.component";
-
-export class DraggableColumnView extends DragView<ColumnState> {
-    constructor(state: ColumnState) {
-        super(state);
-    }
-
-    protected _render(fragment: DocumentFragment): void {
-        const columnComponent = (this.createComponent(this.container, ColumnComponent, this.state) as ColumnComponent);
-        columnComponent.eventEmitter.on('column-updated', (column: Column) => this.eventEmitter.emit('column-updated', column));
-
-        const dragState = this.getRequiredState<DragState>(DragState.name);
-        dragState.updateByKey('draggableArea', columnComponent.view.draggableArea, false);
-
-        super._render(fragment);
     }
 }
