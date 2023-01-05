9beb3978cfad9ddd58c17a322a00bc330192eebb
Eldar Iusupzhanov <eldar.iusupzhanov@devexpress.com>
2023-01-06 03:57:32 +0800

    fill with content

--- src/index.ts
+++ src/index.ts
@@ -31,32 +31,118 @@ window.addEventListener("load", () => {
     const container = document.getElementById("kanban");
 
     const config: KanbanOptions = {
+        // columns: [{
+        //     name: 'In Progress',
+        //     id: 0,
+        //     cards: [
+        //         { id: 0, name: 'card 1' }, { id: 1, name: 'card 2' }, { id: 2, name: 'card 3' },
+        //         { id: 3, name: 'card 1' }, { id: 4, name: 'card 2' }, { id: 5, name: 'card 3' },
+        //         { id: 6, name: 'card 1' }, { id: 7, name: 'card 2' }, { id: 8, name: 'card 3' },
+        //     ]
+        // }, {
+        //     name: 'Done',
+        //     id: 1,
+        //     cards: [
+        //         { id: 18, name: 'card 4' }, { id: 19, name: 'card 5' },
+        //         { id: 25, name: 'card 6 jsnda aksdn lsadm aas as lorem aksldm asdkl asld []wqekr saodj s wek nasda lskdan sdm lasjd kalns' }
+        //     ]
+        // }, {
+        //     name: 'Waiting',
+        //     id: 2,
+        //     cards: [
+        //         { id: 9, name: 'card 1' }, { id: 10, name: 'card 2' }, { id: 11, name: 'card 3' },
+        //         { id: 12, name: 'card 4' }, { id: 13, name: 'card 5' }, { id: 14, name: 'card 6' },
+        //         { id: 15, name: 'card 7' }, { id: 16, name: 'card 8' }, { id: 17, name: 'card 9' },
+        //         { id: 20, name: 'card 10' },
+        //         { id: 21, name: 'card 11' }, { id: 22, name: 'card 12' }, { id: 23, name: 'card 13' },
+        //         { id: 24, name: 'card 14' },
+        //     ]
+        // }]
         columns: [{
-            name: 'In Progress',
+            name: 'Plans for day',
             id: 0,
             cards: [
-                { id: 0, name: 'card 1' }, { id: 1, name: 'card 2' }, { id: 2, name: 'card 3' },
-                { id: 3, name: 'card 1' }, { id: 4, name: 'card 2' }, { id: 5, name: 'card 3' },
-                { id: 6, name: 'card 1' }, { id: 7, name: 'card 2' }, { id: 8, name: 'card 3' },
+                { id: 0, name: 'Learn chineese 1 hour' },
+                { id: 1, name: 'Go to gym' },
+                { id: 2, name: 'Prepare to Statistics exam' },
+                { id: 3, name: 'Throw trash' },
+                { id: 4, name: 'Wash clothes' },
+                { id: 10, name: 'Complete Linux final project' },
             ]
         }, {
-            name: 'Done',
+            name: 'Plans for week',
             id: 1,
             cards: [
-                { id: 18, name: 'card 4' }, { id: 19, name: 'card 5' },
-                { id: 25, name: 'card 6 jsnda aksdn lsadm aas as lorem aksldm asdkl asld []wqekr saodj s wek nasda lskdan sdm lasjd kalns' }
+                { id: 5, name: 'Visit Taipei 101' },
+                { id: 6, name: 'Try new food' },
+                { id: 7, name: 'Cook a dish' },
+                { id: 8, name: 'Complete final project' },
+                { id: 9, name: 'Find a part-time job' },
             ]
         }, {
-            name: 'Waiting',
+            name: 'Books to read and films to watch',
             id: 2,
             cards: [
-                { id: 9, name: 'card 1' }, { id: 10, name: 'card 2' }, { id: 11, name: 'card 3' },
-                { id: 12, name: 'card 4' }, { id: 13, name: 'card 5' }, { id: 14, name: 'card 6' },
-                { id: 15, name: 'card 7' }, { id: 16, name: 'card 8' }, { id: 17, name: 'card 9' },
-                { id: 20, name: 'card 10' },
-                { id: 21, name: 'card 11' }, { id: 22, name: 'card 12' }, { id: 23, name: 'card 13' },
-                { id: 24, name: 'card 14' },
+                { id: 11, name: '451 degrees fahrenheit' },
+                { id: 12, name: 'State. Plato' },
+                { id: 13, name: '1986' },
+                { id: 14, name: 'Hunger Games' },
+                { id: 15, name: 'Lord of the rings' },
+                { id: 16, name: 'Avatar 2' },
+                { id: 17, name: 'Doctor Strange 2' },
+                { id: 18, name: 'Into the Spider-verse' },
+                { id: 19, name: 'Black panther' },
+                { id: 20, name: 'Avengers' },
+                { id: 21, name: 'Bullet train' },
             ]
+        }, {
+            name: 'Favorite anime',
+            id: 3,
+            cards: [
+                { id: 22, name: 'Attack on Titan' },
+                { id: 23, name: 'Death note' },
+                { id: 24, name: 'Code Geass' },
+                { id: 25, name: 'The golden deity' },
+                { id: 26, name: 'The future diary' },
+                { id: 27, name: 'Reincarnation' },
+                { id: 28, name: 'Code Geass 2' },
+                { id: 29, name: 'Blame!' },
+                { id: 30, name: 'Fairy Tail' },
+                { id: 31, name: 'Jojo' },
+                { id: 32, name: 'Game of friends' },
+            ]
+        }, {
+            name: 'Motivation',
+            id: 4,
+            cards: [
+                { id: 33, name: 'Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too' },
+                { id: 34, name: 'When you give joy to other people, you get more joy in return. You should give a good thought to happiness that you can give out' },
+                { id: 35, name: 'It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest' },
+                { id: 36, name: 'We cannot solve problems with the kind of thinking we employed when we came up with them' },
+                { id: 37, name: 'Learn as if you will live forever, live like you will die tomorrow' },
+                { id: 38, name: 'When you change your thoughts, remember to also change your world' },
+                { id: 39, name: 'Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together' },
+                { id: 40, name: 'Develop success from failures. Discouragement and failure are two of the surest stepping stones to success' },
+                { id: 41, name: 'There are three ways to ultimate success: The first way is to be kind. The second way is to be kind. The third way is to be kind.' },
+                { id: 42, name: 'Success is peace of mind, which is a direct result of self-satisfaction in knowing you made the effort to become the best of which you are capable' },
+                { id: 43, name: 'Success is getting what you want, happiness is wanting what you get' },
+            ]
+        }, {
+            name: 'Empty column 1',
+            id: 5,
+            cards: []
+        }, {
+            name: 'Empty column 2',
+            id: 6,
+            cards: []
+        }, {
+            name: 'Empty column 3',
+            id: 7,
+            cards: []
+        }, {
+            name: 'Empty column 4',
+            id: 8,
+            cards: []
         }]
     }
 
